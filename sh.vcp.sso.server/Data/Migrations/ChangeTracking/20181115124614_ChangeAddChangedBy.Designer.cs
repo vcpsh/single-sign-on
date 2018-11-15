b42f1a012ea0f57858ab09eeffc7b7aecd4ae534
mkay-sh <code@mkay-sh.de>
2018-11-15 13:48:14 +0100

    DbMigration for changedbcontext

--- sh.vcp.sso.server/Data/Migrations/ChangeTracking/20181115124614_ChangeAddChangedBy.Designer.cs
+++ sh.vcp.sso.server/Data/Migrations/ChangeTracking/20181115124614_ChangeAddChangedBy.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sh.vcp.ldap.ChangeTracking;
 
 namespace sh.vcp.sso.server.Migrations
 {
     [DbContext(typeof(ChangeTrackingDbContext))]
-    partial class ChangeTrackingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181115124614_ChangeAddChangedBy")]
+    partial class ChangeAddChangedBy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ sh.vcp.sso.server/Data/Migrations/ChangeTracking/20181115124614_ChangeAddChangedBy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sh.vcp.sso.server.Migrations
+{
+    public partial class ChangeAddChangedBy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "changed_by",
+                table: "Changes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "changed_by",
+                table: "Changes");
+        }
+    }
+}
