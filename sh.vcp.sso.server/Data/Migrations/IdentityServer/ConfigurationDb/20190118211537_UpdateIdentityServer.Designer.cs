2ecbdbc854fe141195605784d9ebc06259fa8eb3
mkay-sh <code@mkay-sh.de>
2019-01-18 22:16:33 +0100

    Added migrations for IdentityServer update

--- sh.vcp.sso.server/Data/Migrations/IdentityServer/ConfigurationDb/20190118211537_UpdateIdentityServer.Designer.cs
+++ sh.vcp.sso.server/Data/Migrations/IdentityServer/ConfigurationDb/20190118211537_UpdateIdentityServer.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace sh.vcp.sso.server.Data.Migrations.IdentityServer.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190118211537_UpdateIdentityServer")]
+    partial class UpdateIdentityServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- sh.vcp.sso.server/Data/Migrations/IdentityServer/PersistedGrantDb/20190118211501_UpdateIdentityServer.Designer.cs
+++ sh.vcp.sso.server/Data/Migrations/IdentityServer/PersistedGrantDb/20190118211501_UpdateIdentityServer.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace sh.vcp.sso.server.Data.Migrations.IdentityServer.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190118211501_UpdateIdentityServer")]
+    partial class UpdateIdentityServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
